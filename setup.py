e431db4617231508cd5c83035a92e97e839b19a7
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-11-24 17:43:08 +0100

    Back to development

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-appypod',
-    version='2.0.6',
+    version='2.0.7.dev0',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-appypod/',
