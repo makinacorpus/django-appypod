602574854cb77ed3570df906d1efa1b39d5bfacd
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2020-11-24 17:38:53 +0100

    Preparing release 2.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-appypod',
-    version='2.0.6.dev0',
+    version='2.0.6',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-appypod/',
@@ -16,7 +16,7 @@ setup(
     license='LPGL, see LICENSE file.',
     install_requires=[
         'Django',
-        'appy-python-3@git+ssh://git@github.com/GeotrekCE/appy-python-3@b3f31e8c0c1e1dd774a526d3c1e0a5b48bdb43d7',
+        'appy-python-3>=2020.11.24',
     ],
     packages=find_packages(),
     include_package_data = True,
